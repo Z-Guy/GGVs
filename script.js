2084f440ec28d24682cce6e7c399dc6b8e4e8ead
Christian Tsegaye <C.Tsegaye@outlook.com>
2023-02-05 16:29:11 -0500

    Added a function to compare the value of the characters. The more valued one should win.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const faust = 5;
-const may = 5;
-const hc = 7;
-const baiken = 3;
+const faust = {
+  name: "Faust",
+  value: 5
+};
+const may = {
+  name: "May",
+  value: 5
+};
+const hc = {
+  name: "Happy Chaos",
+  value: 7
+};
+const baiken = {
+  name: "Baiken",
+  value: 3
+};
 
 document.getElementById("pickOpponent").addEventListener("click", function() {
     try {
@@ -10,6 +22,27 @@ document.getElementById("pickOpponent").addEventListener("click", function() {
       console.error(error);
     }
   });
-  
+
+function whoseBetter(a, b){
+  return a.value > b.value 
+    ? a.name + " wins!!!" 
+    : b.value > a.value  
+    ? b.name + " wins!!!" 
+    : "Draw!!";
+}
+/* For when it goes live
+let person = prompt("Select your character:");
+let opponent = prompt("Select your opponent:");
+8?
+*/
+
+//get to the command line
+const person = process.argv[2];
+const opponent = process.argv[3];
+console.log(`Your character: ${person}`);
+console.log(`Your opponent: ${opponent}`);
+
+
+console.log(whoseBetter(person, opponent));
 
 
