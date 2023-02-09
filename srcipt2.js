41601c76d4dbc3e849d1dcc5e1e1155fc43813a2
Christian Tsegaye <C.Tsegaye@outlook.com>
2023-02-09 18:53:46 -0500

    ok

--- srcipt2.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("startMatch").addEventListener("click", function() {
-    try {
-      window.location.href = "Fight.html";
-    } catch (error) {
-      console.error(error);
-    }
-  });
