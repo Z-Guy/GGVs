fe2984dbce193c22d798fedfb8bc7a5a530a9ebb
Christian Tsegaye <C.Tsegaye@outlook.com>
2023-02-09 18:47:12 -0500

    Decide to use links in HTML for the buttons Deleted the two other JavaScript files

--- reset.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("reset").addEventListener("click", function() {
-    try {
-      window.location.href = "Index.html";
-    } catch (error) {
-      console.error(error);
-    }
-  });
